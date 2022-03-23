b2dcd9b2e8888f558d1667215a35cb70b7232474
= <=>
2022-03-23 16:43:34 +0100

    encore

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -16,14 +16,14 @@ namespace UPJV\Validator;
  * Class EstValide implémente les méthode build & check
  */
 class EstValide implements ValidatorInterface
-{ 
+{
     /**
      * Regarde si le mot est une chaine de caractère
-     * 
+     *
      * @param array $param
-     * 
+     *
      * @return objet
-     */  
+     */
     public function build(array $param): object
     {
         return $this;
@@ -33,9 +33,9 @@ class EstValide implements ValidatorInterface
       * Test la longueur de la chaine de caractères
       *
       * @param $input
-      * 
+      *
       * @return function
-      */  
+      */
     public function check($input): bool
     {
         return true;
